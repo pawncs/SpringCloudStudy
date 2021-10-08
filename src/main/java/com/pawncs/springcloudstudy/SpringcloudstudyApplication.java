3c504ef051d2185273a46fc95841c1f8383a9c8d
pawncs <1239048739@qq.com>
2021-10-08 15:37:49 +0800

    qqq

---
+++ src/main/java/com/pawncs/springcloudstudy/SpringcloudstudyApplication.java
@@ -0,0 +1,13 @@
+package com.pawncs.springcloudstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudstudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudstudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pawncs/springcloudstudy/SpringcloudstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pawncs.springcloudstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudstudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
